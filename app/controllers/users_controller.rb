f473b24ac839f724881f5fc887853730b9e8c7c4
Emilia <emilia.skrzypczak.es@gmail.com>
2023-04-26 11:09:45 +0200

    add #edit & #update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,20 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(params_user)
+      redirect_to @user
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
   def params_user
     params.require(:user).permit(:username, :email, :password)
